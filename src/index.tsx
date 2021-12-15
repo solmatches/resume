826605fd433c20d4a40f9ee21a9255722df75645
Mika <mikaboo@jupitaka.com>
2021-12-15 18:44:29 +0900

    feat: 웹폰트 추가 및 적용
    - NotoSans 에셋 추가
    - GlobalStyle 컴포넌트 생성
      - FontFaceObserver 적용 (FOUT 방식으로 작동되게 함)
      - css Normalize 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyles } from './global-styles';
 
 ReactDOM.render(
-    <React.StrictMode>첫페이지!</React.StrictMode>,
+    <React.StrictMode>
+        <GlobalStyles fallback={<>폰트 로딩중...</>}>첫 페이지!</GlobalStyles>
+    </React.StrictMode>,
     document.getElementById('root'),
 );
