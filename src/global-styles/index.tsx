73ed2064d88073ccf1d01fc0b8686e6b8318c04b
Mika <solinepik@gmail.com>
2021-12-15 18:44:29 +0900

    feat: 웹폰트 추가 및 적용
    - NotoSans 에셋 추가
    - GlobalStyle 컴포넌트 생성
      - FontFaceObserver 적용 (FOUT 방식으로 작동되게 함)
      - css Normalize 설정

---
+++ src/global-styles/index.tsx
@@ -0,0 +1,141 @@
+import { css, Global } from '@emotion/react';
+import FontFaceObserver from 'fontfaceobserver';
+import React, { FC, useEffect, useState } from 'react';
+import { useRef } from 'react';
+import { notoSansFontFamily, notoSansKrFontFamily } from './font';
+
+interface Props {
+    fallback: React.ReactElement;
+}
+
+const FONT_FAMILIES = ['-apple-system', '"Noto Sans"', 'sans-serif'];
+
+const styles = () => css`
+    html,
+    body,
+    div#root {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    html {
+        box-sizing: border-box;
+        font-size: 62.5%;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        line-height: 1.15;
+    }
+    body {
+        -webkit-touch-callout: none;
+        margin: 0;
+        background: #fff;
+        color: #121212;
+        font-family: ${FONT_FAMILIES.join(',')};
+        font-size: 1.6rem;
+        font-weight: 400;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p {
+        margin: 0;
+        user-select: none;
+    }
+    ol,
+    ul,
+    li {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        user-select: none;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+        user-select: none;
+    }
+    a,
+    button {
+        outline: none;
+        font-family: inherit;
+        cursor: pointer;
+        -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+    }
+    html,
+    body,
+    div,
+    span,
+    nav,
+    header,
+    footer,
+    menu,
+    main,
+    table,
+    thead,
+    tbody,
+    tfoot,
+    tr,
+    td,
+    th,
+    form,
+    label {
+        user-select: none;
+    }
+
+    input,
+    textarea {
+        box-shadow: none;
+        -webkit-user-select: auto;
+        -moz-user-select: text;
+    }
+
+    *,
+    *:before,
+    *:after {
+        -webkit-tap-highlight-color: transparent;
+        box-sizing: inherit;
+    }
+    *:focus {
+        outline: none !important;
+    }
+
+    ${notoSansFontFamily}
+    ${notoSansKrFontFamily}
+`;
+
+export const GlobalStyles: FC<Props> = ({ fallback, children }) => {
+    let { current: globalFontLoading } = useRef(false);
+    const [isLoading, setIsLoading] = useState(globalFontLoading);
+
+    useEffect(() => {
+        if (isLoading) {
+            const notoSans = new FontFaceObserver('Noto Sans');
+
+            Promise.all([notoSans.load()]).then(() => {
+                globalFontLoading = false;
+                setIsLoading(false);
+            });
+        }
+    }, []);
+
+    if (isLoading) {
+        return (
+            <>
+                <Global styles={styles} />
+                {fallback}
+            </>
+        );
+    }
+
+    return (
+        <>
+            <Global styles={styles} />
+            {children}
+        </>
+    );
+};
