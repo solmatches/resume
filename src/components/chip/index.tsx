16cbc5abe5d2e955d5e437ab1db0157ade36373e
Mika <solinepik@gmail.com>
2021-12-17 05:24:57 +0900

    feat: Chips 컴포넌트 추가
    Skills 페이지에서 사용하던 Chips를 공용으로 사용할 수 있도록 분리 및 변경사항 적용

---
+++ src/components/chip/index.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { css, SerializedStyles } from '@emotion/react';
+import styled from '@emotion/styled';
+import React, { FC, ReactElement } from 'react';
+import { THEME_COLOR } from '~/global-styles/constants';
+
+interface Props {
+    chips: string[] | ReactElement[];
+    css?: SerializedStyles;
+}
+
+export const Chip = styled.div<Pick<Props, 'css'>>`
+    display: inline-block;
+    padding: 0.5em 0.8em;
+    border-radius: 1em;
+    background: ${THEME_COLOR.mono1};
+    font-size: 1.2rem;
+    transition: transform 0.3s;
+
+    ${(props) => props.css}
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.8rem;
+`;
+
+export const Chips: FC<Props> = ({ chips, css: inheritedCSS }) => (
+    <Wrapper css={inheritedCSS}>
+        {chips.map((chip, index) => {
+            if (typeof chip === 'string') {
+                return <Chip key={index}>{chip}</Chip>;
+            }
+            return (
+                <Chip
+                    key={index}
+                    css={css`
+                        border: 1px solid ${THEME_COLOR.mono2};
+                    `}
+                >
+                    {chip}
+                </Chip>
+            );
+        })}
+    </Wrapper>
+);
--- src/pages/main/skills.tsx
+++ src/pages/main/skills.tsx
@@ -1,6 +1,7 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { FC } from 'react';
-import { THEME_COLOR } from '~/global-styles/constants';
+import { Chips } from '~/components';
 
 const SKILLS: string[] = [
     'React',
@@ -14,6 +15,11 @@ const SKILLS: string[] = [
     'Rollup',
 ];
 
+const chip = css`
+    > div:hover {
+        transform: scale(1.15);
+    }
+`;
 const Wrapper = styled.article`
     margin-top: 4rem;
 `;
@@ -21,34 +27,12 @@ const Subject = styled.h2`
     margin-bottom: 2rem;
     font-size: 1.2em;
 `;
-const Chips = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.8rem;
-`;
-
-const Chip = styled.div`
-    display: inline-block;
-    padding: 0.5em 0.8em;
-    border-radius: 1em;
-    background: ${THEME_COLOR.mono1};
-    font-size: 1.2rem;
-    transition: transform 0.3s;
-
-    &:hover {
-        transform: scale(1.15);
-    }
-`;
 
 const Skills: FC = () => {
     return (
         <Wrapper>
             <Subject>Skills</Subject>
-            <Chips>
-                {SKILLS.map((skill) => (
-                    <Chip key={skill}>{skill}</Chip>
-                ))}
-            </Chips>
+            <Chips chips={SKILLS} css={chip} />
         </Wrapper>
     );
 };
